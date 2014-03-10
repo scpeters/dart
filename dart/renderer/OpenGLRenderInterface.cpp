119a020f966ed22eac9554512c05222ccec519f1
Jeongseok Lee <jslee02@gmail.com>
2014-03-10 04:38:00 -0400

    Add mekaRobot app

--- dart/renderer/OpenGLRenderInterface.cpp
+++ dart/renderer/OpenGLRenderInterface.cpp
@@ -321,9 +321,9 @@ void OpenGLRenderInterface::applyMaterial(const struct aiMaterial *mtl)
 
     max = 1;
     if((AI_SUCCESS == aiGetMaterialIntegerArray(mtl, AI_MATKEY_TWOSIDED, &two_sided, &max)) && two_sided)
-        glEnable(GL_CULL_FACE);
-    else
         glDisable(GL_CULL_FACE);
+    else
+        glEnable(GL_CULL_FACE);
 }
 
 
